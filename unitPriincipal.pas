abe397ac2bd98ee1fc2c1eefe1db7d279549ab1b
danielcatto <danielcatto@hotmail.com>
2025-01-24 18:09:01 -0300

    criando sistema clinica parte 1 menu e botões

--- unitPriincipal.pas
+++ unitPriincipal.pas
@@ -4,10 +4,21 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons, Vcl.ExtCtrls,
+  Vcl.Menus, Vcl.Imaging.jpeg;
 
 type
   TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    g1: TMenuItem;
+    Sair1: TMenuItem;
+    Cadastros1: TMenuItem;
+    Pacientes1: TMenuItem;
+    Agendamentos1: TMenuItem;
+    Panel2: TPanel;
+    BitBtn2: TBitBtn;
+    BitBtn1: TBitBtn;
+    Image1: TImage;
   private
     { Private declarations }
   public
